afb607281b326038042b4cfded890670c0b16aeb
dedottag <chponga@mail.ru>
2023-07-01 09:50:51 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./img_537094.png";
+import { useState } from "react";
 
 function App() {
+  const [speed, setSpeed] = useState("0");
+  const [light, setLight] = useState(false);
+
+  let style = {
+    width: "500px",
+    height: "500px",
+    transform: `rotate(${speed}deg)`,
+  };
+
+  const styleLight = {
+    backgroundColor: "orange",
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      dedottag
+      <div className="tittle">
+        <h1>Вентилятор, чтоб тебе было не так жарко этим летом</h1>
+      </div>
+      <div className="ventilyator-container">
+        <img className="logo" style={style} alt="" src={logo}></img>
+      </div>
+      <div className="buttons">
+        <ul>
+          <li>
+            <div className="light" style={light ? styleLight : null}>
+              light
+            </div>
+            <button
+              className="light"
+              onClick={() => {
+                setLight(true);
+              }}
+            ></button>
+          </li>
+          <li>
+            <button
+              className="speed3"
+              onClick={() => {
+                setSpeed("2000");
+                console.log(speed);
+              }}
+            >
+              3
+            </button>
+          </li>
+          <li>
+            <button
+              className="speed2"
+              onClick={() => {
+                setSpeed("1000");
+                console.log(speed);
+              }}
+            >
+              2
+            </button>
+          </li>
+          <li>
+            <button
+              className="speed1"
+              onClick={() => {
+                setSpeed("360");
+                console.log(speed);
+              }}
+            >
+              1
+            </button>
+          </li>
+          <li>
+            <button
+              className="speed0"
+              onClick={() => {
+                setSpeed("0");
+                setLight(false);
+                console.log(speed);
+              }}
+            >
+              0
+            </button>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
