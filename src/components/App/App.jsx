3170049bc523c6dd05ce73550721686685802f70
dedottag <chponga@mail.ru>
2023-09-08 22:45:27 +0300

    2.0.1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,22 @@
-import "./App.css";
+import "./App.scss";
 import Header from "../Header";
 import { useState } from "react";
+import Fireplace from "../Fireplace";
 
 function App() {
   const [checked, setCheked] = useState(false);
 
-  const clas = checked ? "dark-app" : "App";
+  if (checked) {
+    document.getElementsByTagName("body")[0].style.background =
+      "rgba(0, 0, 0, 0.754";
+  } else {
+    document.getElementsByTagName("body")[0].style.background = "white";
+  }
 
   return (
-    <div className={clas}>
+    <div className="App">
       <Header checked={checked} setCheked={setCheked} />
+      <Fireplace />
     </div>
   );
 }
---
+++ src/components/Fireplace/Fireplace.jsx
@@ -0,0 +1,8 @@
+const Fireplace = () => {
+  return (
+    <div className="">
+      <h1>Камин</h1>
+    </div>
+  );
+};
+export default Fireplace;
