108396bad2468c21bc227e6a38960acacb681fbf
dedottag <chponga@mail.ru>
2024-01-31 19:35:37 +0300

    changes to the dark theme

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,13 +3,17 @@ import Header from "../Header/Header";
 import MainPage from "../MainPage/MainPage";
 import MainPageMobile from "../MainPageMobile/MainPageMobile";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [dark, setDark] = useState(false);
 
-  const body = document.getElementById("body");
-  body.style.backgroundColor = dark ? "rgb(17, 42, 65)" : "";
+  // const body = document.getElementById("body");
+  // body.style.backgroundColor = dark ? "rgb(17, 42, 65)" : "";
+
+  // useEffect(() => {
+  //   console.log(window.matchMedia("(prefers-color-scheme: dark)").matches);
+  // }, [window.matchMedia("(prefers-color-scheme: dark)").matches]);
 
   return (
     <div className="App">
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,33 +1,30 @@
 import "./header.scss";
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import sunIcon from "../IconsList/icons/sunIcon.svg";
 import moonIcon from "../IconsList/icons/moonIcon.svg";
 
-const Header = ({ dark, setDark }) => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+const Header = () => {
+  // const [isDarkMode, setIsDarkMode] = useState(false);
 
-  const toggleDarkMode = () => {
-    setIsDarkMode((prevState) => !prevState);
-  };
+  // const toggleDarkMode = () => {
+  //   setIsDarkMode((prevState) => !prevState);
+  // };
 
-  useEffect(() => {
-    const storedIsDarkMode = localStorage.getItem("isDarkMode") === "true";
-    setIsDarkMode(storedIsDarkMode);
-  }, []);
+  // useEffect(() => {
+  //   const storedIsDarkMode = localStorage.getItem("isDarkMode") === "true";
+  //   setIsDarkMode(storedIsDarkMode);
+  // }, []);
 
-  useEffect(() => {
-    localStorage.setItem("isDarkMode", isDarkMode);
-    setDark(isDarkMode);
-  }, [isDarkMode]);
+  // useEffect(() => {
+  //   localStorage.setItem("isDarkMode", isDarkMode);
+  //   setDark(isDarkMode);
+  // }, [isDarkMode]);
 
   return (
-    <header className={`header-container ${dark ? "header-dark" : ""}`}>
+    <header className={`header-container`}>
       <h2>dedottag</h2>
-      <img
-        src={!isDarkMode ? moonIcon : sunIcon}
-        alt="icon"
-        onClick={() => toggleDarkMode()}
-      />
+      <img className="moon" src={moonIcon} alt="icon" />
+      <img className="sun" src={sunIcon} alt="icon" />
     </header>
   );
 };
