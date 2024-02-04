ab2da18663c2d8063cfcedd119b7ab1f86749227
dedottag <chponga@mail.ru>
2024-02-04 14:50:08 +0300

    fix6

--- src/components/MySkills/MySkills.jsx
+++ src/components/MySkills/MySkills.jsx
@@ -22,26 +22,19 @@ const MySkills = () => {
   const containerRef3 = useRef(null);
 
   const scrollContainerToMiddle = (containerRef) => {
+    console.log(containerRef.current.scrollHeight);
     if (containerRef.current) {
       const container = containerRef.current;
       const scrollHeight = container.scrollHeight;
       const clientHeight = container.clientHeight;
       container.scrollTop = (scrollHeight - clientHeight) / 2;
+      console.log(containerRef.current.scrollTop);
     } else {
       console.error("Container element not found");
     }
   };
 
   useEffect(() => {
-    // const scrollContainerToMiddle = (containerRef) => {
-    //   if (containerRef.current.children !== null) {
-    //     console.log(containerRef);
-    //     const container = containerRef?.current;
-    //     const containerHeight = container.scrollHeight;
-    //     container.scrollTop =
-    //       containerHeight / 2 - container.getBoundingClientRect().height / 2;
-    //   }
-    // };
     scrollContainerToMiddle(containerRef1);
     scrollContainerToMiddle(containerRef2);
     scrollContainerToMiddle(containerRef3);
